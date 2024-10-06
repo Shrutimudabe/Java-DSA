cf1fa2c183d78874f4f8fc2068dd8fc5526564d1
Shrutimudabe <mudabeshruti@gmail.com>
2024-10-07 00:07:30 +0530

    palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0,rem = 0;
+        int n = x;
+        while(n>0){
+          rem = n%10;
+          rev = rev*10+rem;
+            n = n/10;  
+        }
+        if(rev==x){
+            return true ;
+        }
+        return false;
+    }
+}
