aa484b91a5b12f987ec0ad4f12189b941716c5c7
Shrutimudabe <mudabeshruti@gmail.com>
2025-02-25 11:42:04 +0530

    All Indices in an Array1.java

---
+++ Recursion/All Indices in an Array1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void allIndicesInArray(int[]arr,int i,int x){
+         if(i==arr.length) return;
+        
+        if(arr[i]==x){
+            System.out.println(i);
+        }
+        allIndicesInArray(arr,i+1,x);
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+         int n = sc.nextInt();
+         int arr[] = new int[n];
+        
+        for(int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+         int x = sc.nextInt();
+        allIndicesInArray(arr,0,x);
+    }
+}
